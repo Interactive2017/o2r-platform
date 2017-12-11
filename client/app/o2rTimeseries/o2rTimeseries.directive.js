f1ebb0a34e55d1c3d87f8682023211fc1ba51f6b
LEinfeldt <l_einf01@uni-muenster.de>
2017-12-11 16:32:17 +0100

    Add data for layout (commented)

--- client/app/o2rTimeseries/o2rTimeseries.directive.js
+++ client/app/o2rTimeseries/o2rTimeseries.directive.js
@@ -10,17 +10,29 @@
         return{
 			restrict: 'E',
             templateUrl: 'app/o2rTimeseries/o2rTimeseries.template.html',
-            link: link
+            link: link  
+            // scope: {
+            //     data: '=o2rData',
+            //     layout = '=o2rTitle'
         }
-    function link(scope, element, attrs){
-         scope.data = [{
-             x: [1, 2, 3, 4, 5],
-             y: [1, 2, 4, 8, 16]
-          }]  
-    }
-    
-    //     
 
-    }  
+    }
+
+    function link(scope, element, attrs){
+        var title;
+        // attrs.$observe('data', function(value){
+        //     scope.data = angular.fromJson(value);   
+        // });
+
+        // attrs.$oberve('title', function(value) {
+        //     title = angular.fromJson(value);
+        // })
+
+        scope.data = [{
+            x: [1, 2, 3, 4, 5],
+            y: [1, 2, 4, 8, 16]
+        }]
+        scope.layout = {title: title};
+        scope.options = {showLink: false, displayLogo: false};
+    }
 })();
-    
