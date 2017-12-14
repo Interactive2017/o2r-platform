e518bf1e40900814c71dbc2b917f0f95a9354921
Torben <t.kraf03@gmail.com>
2017-12-14 18:46:13 +0100

    add interface for uploading package

--- client/app/ercView/erc.controller.js
+++ client/app/ercView/erc.controller.js
@@ -5,13 +5,13 @@
         .module('starter')
         .controller('ErcController', ErcController);
 
-    ErcController.$inject = ['$scope', '$stateParams', '$log', '$state', 'erc', 'publications', 'icons', 'header', '$mdSidenav', 'env', 'ngProgressFactory', 'httpRequests', 'login'];
-    function ErcController($scope, $stateParams, $log, $state, erc, publications, icons, header, $mdSidenav, env, ngProgressFactory, httpRequests, login){
+    ErcController.$inject = ['$scope', '$stateParams', '$log', '$state', '$mdDialog', 'erc', 'publications', 'icons', 'header', '$mdSidenav', 'env', 'ngProgressFactory', 'httpRequests', 'login'];
+    function ErcController($scope, $stateParams, $log, $state, $mdDialog, erc, publications, icons, header, $mdSidenav, env, ngProgressFactory, httpRequests, login){
         var logger = $log.getInstance('ErcCtrl');
         var defView = {};
         defView.state = 'erc.reproduce';
         defView.name = 'reproduce';
-        
+
         var vm = this;
         vm.icons = icons;
         vm.server = env.server;
@@ -50,6 +50,7 @@
         vm.publish = true;
         vm.sendToZenodo = sendToZenodo;
         vm.publishInZenodo = publishInZenodo;
+        vm.uploadPackage = uploadPackage;
 
         logger.info(vm.publication);
 
@@ -63,11 +64,28 @@
             getShipment();
         }
 
+        // ------------------------------------------------------------
+
+        function uploadPackage(ev, data){
+							$mdDialog.show({
+                  controller: 'UploadPackageController',
+                  controllerAs: 'vm',
+                  templateUrl: 'app/uploadPackage/uploadPackage.html',
+									parent: angular.element(document.body),
+									targetEvent: ev,
+									fullscreen: true,
+									clickOutsideToClose: false,
+									multiple: true
+							});
+					}
+
+        // ------------------------------------------------------------
+
         function getShipment(){
             httpRequests.getShipment(vm.ercId)
                 .then(function (res){
                     logger.info(res);
-                    if(res.data.length > 0){ 
+                    if(res.data.length > 0){
                         vm.shipped=true;
                         httpRequests.getStatus(res.data[0])
                         .then(function (res2){
@@ -77,7 +95,7 @@
                             }
                             if (res2.data.status == "published"){
                                 vm.publish = true;
-                            }    
+                            }
                         })
                         .catch(function (err2){
                             logger.info(err2);
@@ -103,7 +121,7 @@
 			})
             .catch(function (err){
                 logger.info(err);
-            })     
+            })
         }
 
         function publishInZenodo(){
@@ -111,8 +129,8 @@
                 .then(function (res){
                     httpRequests.publishERC(res.data[0])
                     .then(function (res2){
-                        logger.info("published")  
-                        logger.info(res2) 
+                        logger.info("published")
+                        logger.info(res2)
                     })
                     .catch(function (err2){
                         logger.info(err2);
@@ -153,8 +171,8 @@
         }
 
         /**
-         * 
-         * @param {Object} obj, expects an object with two attributes: code, data. Each attribute is an array 
+         *
+         * @param {Object} obj, expects an object with two attributes: code, data. Each attribute is an array
          */
         function mSetCodeData(obj){
             vm.mCodeData = obj;
---
+++ client/app/uploadPackage/uploadPackage.controller.js
@@ -0,0 +1,187 @@
+(function(){
+    'use strict';
+
+    angular
+        .module('starter')
+        .controller('UploadPackageController', UploadPackageController);
+
+    UploadPackageController.$inject = ['$scope', '$stateParams', '$log', '$mdDialog', 'icons', '$q', 'publications', 'httpRequests'];
+    function UploadPackageController($scope, $stateParams, $log, $mdDialog, icons, $q, publications, httpRequests){
+        var logger = $log.getInstance('UploadPackageCtrl');
+        var vm = this;
+
+        vm.icons = icons;
+        vm.cancel = cancel;
+        vm.simpleUpdate = simpleUpdate;
+        vm.showError = showError;
+        vm.savePackage = savePackage;
+        vm.addWidget = addWidget;
+        vm.required = {};
+        vm.required.widgets = {};
+        vm.packageData = {};
+        vm.packageData.widgets = [];
+        $scope.widgetModel = {};
+        $scope.widgetModel.type = "slider";
+
+        logger.info('UploadPackageCtrl with params:', $stateParams);
+
+        /////
+
+        // bool: true stands for widget, false stands for figure
+        function simpleUpdate(attr, val, bool){
+          if (bool) {
+            vm.required.widgets[attr] = val;
+            logger.info('updated ', attr);
+          } else {
+            vm.required[attr] = val;
+            logger.info('updated ', attr);
+          }
+        };
+
+        function showError(fieldname){
+            var field = $scope.requiredForm[fieldname];
+
+            // if tab was switched and form is invalid OR input was touched and is invalid show error message in input
+            if((field.$invalid) || (field.$touched && field.$invalid)) return true;
+            else return false;
+        };
+
+        function savePackage() {
+            if (checkForFigureValidation()) {
+                var reqData = vm.required;
+                vm.packageData.figure_id = reqData.figure_id;
+                vm.packageData.type = reqData.figure_type;
+                vm.packageData.endpoint = reqData.figure_endpoint;
+
+                var addWidgetValue = addWidget();
+                // check if widget inputfields are filled
+                if (!addWidgetValue.bool) {
+                    // check if widget array is at least filled with one object
+                    if (vm.packageData.widgets.length <= 0) {
+                        logger.info("Save not possible:\nWidget field:"+addWidgetValue.field+" not filled\nand no widgets were added.");
+                        alert("Save not possible:\nWidget field:"+addWidgetValue.field+" not filled\nand no widgets were added before.");
+                    } else {
+                        logger.info(vm.packageData);
+                        let passon = {
+                            pkg: vm.packageData
+                        }
+                        savePkgToDB(passon)
+                          .then(function(res) {
+                              logger.info(res);
+                              if (res.error == undefined) {
+                                  $mdDialog.cancel();
+                              } else {
+                                  alert("Save not successfull.\n"+res.error+".");
+                              }
+                          })
+                          .catch(function(err){
+                              logger.info(err);
+                              return {error: "SAVE metadata of choosen ERC is not possible."};
+                          });
+                    }
+                } else {
+                    logger.info(vm.packageData);
+                    let passon = {
+                        pkg: vm.packageData
+                    }
+                    savePkgToDB(passon)
+                      .then(function(res) {
+                          logger.info(res);
+                          if (res.error == undefined) {
+                              $mdDialog.cancel();
+                          } else {
+                              alert("Save not successfull.\n"+res.error+".");
+                          }
+                      })
+                      .catch(function(err){
+                          logger.info(err);
+                          return {error: "SAVE metadata of choosen ERC is not possible."};
+                      });
+                }
+            } else {
+                logger.info("Save not possible:\nAt least one required figure inputfield is not filled.");
+                alert("Save not possible:\nAt least one required figure inputfield is not filled.");
+            }
+        };
+
+        function cancel() {
+          resetPackage();
+          $mdDialog.cancel();
+        };
+
+        function savePkgToDB(passon) {
+            return getMetadata()
+              .then(function(res){
+                  logger.info(res);
+                  if (res.metadata.o2r.interaction == undefined || res.metadata.o2r.interaction.length == undefined) {
+                      res.metadata.o2r.interaction = [];
+                  }
+                  res.metadata.o2r.interaction.push(passon.pkg);
+
+                  return httpRequests.updateMetadata($stateParams.ercid, res.metadata.o2r)
+                    .then(function(res){
+                        logger.info(res);
+                        return res;
+                    })
+                    .catch(function(err){
+                        logger.info(err);
+                        return {error: "UPDATE metadata of choosen ERC is not possible."};
+                    });
+              })
+              .catch(function(err){
+                  logger.info(err);
+                  return {error: "GET metadata of choosen ERC is not possible."};
+              });
+        };
+
+        function getMetadata() {
+          let ercId = $stateParams.ercid;
+          return publications.getRequest(ercId).then(function(result){
+              if(result.status == 404){
+                  return $q.reject('404 Not Found');
+              }
+              else {
+                  return result;
+              }
+          });
+        };
+
+        function addWidget() {
+            var checkWidgetInput = checkForWidgetInputValues();
+            if (!checkWidgetInput.bool) {
+                logger.info("please fill in every widget field. At least:\n -> " + checkWidgetInput.field);
+                return {bool: false, field: checkWidgetInput.field};
+            } else {
+              var reqData = vm.required.widgets;
+              reqData.type = 'slider';
+              vm.packageData.widgets.push(reqData);
+              vm.required.widgets = {};
+              return {bool: true};
+            }
+        };
+
+        function checkForWidgetInputValues() {
+            var arrayOfWidgetModels = ['default_value', 'min_value', 'max_value', 'steps_size', 'param_name', 'description']; // without 'type', because it is always = 'slider'
+
+            for (let i=0; i< arrayOfWidgetModels.length; i++) {
+                if (vm.required.widgets[arrayOfWidgetModels[i]] == undefined) {
+                    return {bool: false, field: arrayOfWidgetModels[i]};
+                }
+            }
+            return {bool: true};
+        };
+
+        function checkForFigureValidation() {
+            if ($scope.requiredForm.$valid) {
+                return true;
+            } else {
+                return false;
+            }
+        }
+
+        function resetPackage() {
+            vm.required = {};
+            vm.packageData = {};
+        }
+    }
+})()
