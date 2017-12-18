56e3beb72e6a74d740207c4978b58733176ec488
Antonia <antoniaicq@web.de>
2017-12-18 12:12:56 +0100

    New metadata structure and slider functionality (not yet handling slider events).

--- client/app/mainCompare/mainCompare.controller.js
+++ client/app/mainCompare/mainCompare.controller.js
@@ -8,16 +8,16 @@
     mainCompareController.$inject = ['$scope', '$log', 'erc', 'icons'];
     function mainCompareController($scope, $log, erc, icons){
 
-        // TODO: make sure that page is loaded completely
-
         var logger = $log.getInstance('mainCompare');
-        var compare = erc;
         var vm = this;
-        vm.selectedTab = 0;
-        vm.figures = compare.metadata.o2r.interaction;
-        logger.info(vm.figures);
+        vm.initialTab = 0;
+        var compare = erc;
+        var first = true;
+        $scope.icons = icons;   
+        vm.figures = compare.metadata.o2r.interaction;                
         
-        /** Initial compare type selection */
+        
+        // compare type selction types
         $scope.mapCompareTypes = [
             "Side-by-side",
             "Overlay",
@@ -28,44 +28,40 @@
             "Combined"
         ];
 
-        // At the beginning the first figure is loaded
-        vm.compareType = compare.metadata.o2r.interaction[0].type;
-        vm.widgets = compare.metadata.o2r.interaction[0].widgets;
-        logger.info(vm.widgits);
-
-        // TODO:
-        /** Initial slider configuration */
-         // auf typ checken bevor ich die in das neue array mit aufnehme
-         vm.slider = {
-            value: 200,
-            options: {
-                floor: 0,
-                ceil: 500
+        // function to initialize the slider
+        $scope.initializeSlider = function(figure){
+            vm.widgets = compare.metadata.o2r.interaction[figure].widgets;
+            for(var widget in vm.widgets){
+                vm.widgets[widget].value = vm.widgets[widget].default_value; // set default value
+                vm.widgets[widget].options = {floor: vm.widgets[widget].min_value, ceil: vm.widgets[widget].max_value }; // set min value
             }
+        }
+
+        // function to show comparison visulization
+        $scope.changeVisualization = function(type){
+            // TODO show visualization
         };
+        
 
-
-        $scope.icons = icons;
-
-        //when another tab/figure has been selected by the user
+        // Load figure when tab was changed
+        /** another tab/figure has been selected by the user */
         $scope.$watch('vm.selectedTab', function(newVal, oldVal){
-            //logger.info('Tab changed to object: %s', newVal);
-            // newVal is the array index of the current figure
+            if(first){
+                first = false;
+            }else if (!first){
+                logger.info("Changed Tab");
 
-            //TODO set new comparison type
-            $scope.compareType = "map"; 
+                // set new comparison type
+                vm.compareType = compare.metadata.o2r.interaction[newVal].type;            
 
+                // build new sliders
+                $scope.initializeSlider(newVal);
 
-            //TODO build new sliders
-
-            //TODO draw new visualization?
-
-
+                //TODO draw new visualization?
+                //$scope.changeVisualization();
+            }
         });
 
-
-        $scope.changeVisualization = function(type){
-            // todo show visualization
-        };
+        
     }
 })()
